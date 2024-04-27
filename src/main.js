a943bedbab7a2f2150c569babb287f36d3812861
Анна <anna.kuvakina91@gmail.com>
2024-04-27 11:53:41 +0300

    valid-local

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 const formEl = document.querySelector('.work-form');
 const emailEl = document.querySelector('.work-form input[name=email]');
-const commentsEl = document.querySelector('.work-form input[name=comments]');
+const commentsEl = document.querySelector('.work-form input[name=comment]');
+const succesEl = document.querySelector('.work-form-succes');
+const invalidEl = document.querySelector('.work-form-invalid')
 
+// succesEl.style.display = 'none';
+// invalidEl.style.display = 'none';
+
+//const BASE_URL = 'https://portfolio-js.b.goit.study/api/';
 const localKey = 'work-form-state';
 let formDataEl = {};
 
@@ -10,7 +16,7 @@ fillFormFields();
 formEl.addEventListener(
   'input',
   event => {
-    formDataEl[event.target.name] = event.target.value.trim();
+    formDataEl[event.target.name] = event.target.value;
     localStorage.setItem(localKey, JSON.stringify(formDataEl));
   }
 );
@@ -19,6 +25,28 @@ formEl.addEventListener('submit', event => {
   event.preventDefault();
   if (emailEl.value !== '' && commentsEl.value !== '') {
     console.log(formDataEl);
+
+    // const options = {
+    //     method: "POST",
+    //     body: JSON.stringify(formDataEl),
+    //     headers: {
+    //         "Content-Type": "application/json; charset=UTF-8",
+    //     },
+    //   };
+    
+    //   console.log(options);
+      
+    //   fetch(`${BASE_URL}`, options)
+    //     .then((response) => {
+    //       if (!response.ok) {
+    //         throw new Error(response.status);
+    //       }
+    //       return response.json();
+    //     })
+    //     .then(post => console.log(post))
+    //     .catch(error => console.log(error));
+
+
     localStorage.removeItem(localKey);
     event.currentTarget.reset();
     return;
@@ -31,6 +59,6 @@ function fillFormFields() {
   if (saveFormFields) {
     formDataEl = JSON.parse(saveFormFields);
     emailEl.value = formDataEl.email || '';
-    commentsEl.value = formDataEl.comments || '';
+    commentsEl.value = formDataEl.comment || '';
   }
 }
